b10838e3b9e47fa394232cd1fcc454faf788f3f3
shangyun-sx <1308714960@qq.com>
2026-02-13 10:35:08 +0800

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { rehypeImageWidth } from "./src/plugins/rehype-image-width.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.siteURL,
+	site: "https://shangyun-sx.github.io/mizuki-blog/",
 	base: "/mizuki-blog/",
 	trailingSlash: "always",
 
