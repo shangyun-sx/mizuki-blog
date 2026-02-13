9f58361c317e4870dc913acfdfaec935dd206cc9
shangyun-sx <1308714960@qq.com>
2026-02-13 09:59:22 +0800

    ci: fix github actions for pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ import { rehypeImageWidth } from "./src/plugins/rehype-image-width.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.siteURL,
-	base: "/",
+	base: "/mizuki-blog/",
 	trailingSlash: "always",
 
 	output: "static",
