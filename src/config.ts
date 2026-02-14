355a5309e6572dd0aced08f12fb348bb513803d7
shangyun-sx <1308714960@qq.com>
2026-02-14 11:12:45 +0800

    Fix base path configuration

--- src/config.ts
+++ src/config.ts
@@ -461,7 +461,7 @@ export const announcementConfig: AnnouncementConfig = {
 export const musicPlayerConfig: MusicPlayerConfig = {
 	enable: true, // 启用音乐播放器功能
 	mode: "meting", // 音乐播放器模式，可选 "local" 或 "meting"
-	meting_api: "https://api.injahow.cn/meting/?type=:type&id=:id",// Meting API 地址
+	meting_api: "https://api.injahow.cn/meting/?type=:type&id=:id", // Meting API 地址
 	id: "14164869977", // 歌单ID
 	server: "netease", // 音乐源服务器。有的meting的api源支持更多平台,一般来说,netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
 	type: "playlist", // 播单类型
